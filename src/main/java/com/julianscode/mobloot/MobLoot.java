beefb5c575bf7d8b3ee36c5508004b7b1a9c8d0a
Julian Meyer <julianmeyer2000@gmail.com>
2014-08-18 09:30:59 -0700

    Got eclipse to stop being a derp

--- src/main/java/com/julianscode/mobloot/MobLoot.java
+++ src/main/java/com/julianscode/mobloot/MobLoot.java
@@ -1,14 +1,14 @@
-package com.example.examplemod;
+package com.julianscode.mobloot;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MobLoot.MODID, version = MobLoot.VERSION)
+public class MobLoot
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "mobloot";
     public static final String VERSION = "1.0";
     
     @EventHandler
