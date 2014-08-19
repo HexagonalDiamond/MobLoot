916bf67a1bfdf98e5e5c23911c9308839b6b0113
Julian Meyer <julianmeyer2000@gmail.com>
2014-08-19 11:25:11 -0700

    Finally finished Version 1.0

--- src/main/java/com/julianscode/mobloot/ClientProxy.java
+++ src/main/java/com/julianscode/mobloot/ClientProxy.java
@@ -7,7 +7,7 @@ import com.jadarstudios.developercapes.DevCapes;
 
 public class ClientProxy extends CommonProxy {
 	@Override
-	public void registerConfigs() throws MalformedURLException {
-		DevCapes.getInstance().registerConfig("https://raw.githubusercontent.com/jmeyer2k/MobLoot/master/capes.json");
+	public void registerConfigs() {
+		DevCapes.getInstance().registerConfig("https://dl.dropboxusercontent.com/s/5yz6q8mze86re86/capes.txt?dl=0");
 	}
 }
