ffd0a6c7fa42be6ec1ab31f8f1d63edb636844d2
Julian Meyer <julianmeyer2000@gmail.com>
2014-08-19 11:53:54 -0700

    Forgot multiplayer testing. All done now

--- src/main/java/com/julianscode/mobloot/MobLoot.java
+++ src/main/java/com/julianscode/mobloot/MobLoot.java
@@ -44,7 +44,7 @@ public class MobLoot
     public static final String VERSION = "1.0";
     @Instance(MODID)
     public static MobLoot instance;
-    @SidedProxy(modId=MODID, clientSide="com.julianscode.mobloot.ClientProxy")
+    @SidedProxy(modId=MODID, clientSide="com.julianscode.mobloot.ClientProxy", serverSide="com.julianscode.mobloot.ServerProxy")
     public static CommonProxy proxy;
     public static MobLootConfigManager config;
     public static MobLootGenerator lootGen;
---
+++ src/main/java/com/julianscode/mobloot/ServerProxy.java
@@ -0,0 +1,8 @@
+package com.julianscode.mobloot;
+
+
+public class ServerProxy extends CommonProxy {
+	@Override
+	public void registerConfigs() {
+	}
+}
